b9cf3648adfbe9ee7b599334a73f97bdae7e81ff
Anatoliy2104 <89733031+Anatoliy2104@users.noreply.github.com>
2026-02-19 14:10:41 +0100

    fix

---
+++ Source/ResearchPlayGround/ResearchPlayGround.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ResearchPlayGround.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ResearchPlayGround, "ResearchPlayGround" );
